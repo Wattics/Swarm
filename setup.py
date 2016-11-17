9e5d9154bc6160ce985ea81ace1b7ad9ff128251
Michele Delle Vergini <michele.dellevergini@wattics.com>
2016-11-17 11:16:47 +0000

    Add setuptools config

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""A setuptools based setup module."""
+
+import codecs
+from os import path
+
+from setuptools import find_packages, setup
+
+HERE = path.abspath(path.dirname(__file__))
+
+with codecs.open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name='swarm',
+    version='1.0.0',
+    description='A simple Python project to upload data to the Wattics API',
+    long_description=LONG_DESCRIPTION,
+    url='',
+    author='Michele Delle Vergini',
+    author_email='michele.dellevergini@wattics.com',
+    license='BSD',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[
+        'requests',
+        'tqdm'
+    ],
+    entry_points={
+        'console_scripts': [
+            'swarm=swarm.app:main',
+        ],
+    }
+)
